80bb05ccdd61f39b0f99e302b35a24b674e4e570
Inessa <i.vasileva.stc@innopolis.university.ru>
2020-12-18 17:29:33 +0300

    DZ

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double number1 = 100;
+	    double number2 = 13;
+	    double percent = 10;
+	    Calculator Calculator = new Calculator();
+	    System.out.println("Калькулятор. Результат выполнения операций числа " + number1 +" к числу " + number2);
+        System.out.println("'-' = " +Calculator.Subtraction (number1, number2));
+	    System.out.println("'+' = " +Calculator.adition(number1, number2));
+        System.out.println("'*' = " +Calculator.multiplicatior (number1, number2));
+        System.out.println("'/' = " +String.format("%.2f",Calculator.division(number1, number2)));
+        System.out.println(percent + " % от " + number1 +" = " +Calculator.CetPercentFromnumber(number1, percent));
+
+
+
+    }
+}
